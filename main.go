6f4be2a53f55803854f499e2d1bbc7302d1773e1
ChinmayR <chinmayr@uber.com>
2017-08-15 13:25:05 -0700

    Basic parent package

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ChinmayR/PackageB"
+	"github.com/ChinmayR/PackageC"
+)
+
+func main() {
+	fmt.Print("From Package A :" + PackageB.FuncInPackageB() + PackageC.FuncInPackageC())
+}
