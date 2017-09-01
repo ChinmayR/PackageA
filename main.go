74ad1b753d8cc84ff1840170fbd834e8b8d9182b
ChinmayR <chinmayr@uber.com>
2017-09-01 14:58:57 -0700

    build a dependency on urface/cli

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 
 	"github.com/ChinmayR/PackageB"
 	"github.com/ChinmayR/PackageC"
+	"github.com/urfave/cli"
 )
 
 func main() {
 	fmt.Print("From Package A :" + PackageB.FuncInPackageB() +
 		PackageC.FuncInPackageC())
+
+	app := cli.NewApp()
+	app.Usage = "Build a dependency on it"
 }
