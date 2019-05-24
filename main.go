3e82197c32addb0a281f584663e7a43b0361f520
Chinmay Raval <chinmayr@uber.com>
2019-05-24 13:14:39 -0700

    go modules demo

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ChinmayR/PackageB"
-	"github.com/ChinmayR/PackageC"
 )
 
 func main() {
-	fmt.Print("From Package A :" + PackageB.FuncInPackageB() +
-		PackageC.FuncInPackageC())
+	fmt.Println("Hello from PackageA")
+	return
 }
